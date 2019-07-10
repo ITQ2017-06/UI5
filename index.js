f90e8e5454159c64e698cf0e9ed2361c37e706c1
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-10 11:15:22 +0200

    Test

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-sap.ui.define([], function () {
-    "use strict";
-    alert("UI5 is ready");
-    });
+sap.ui.define([
+	"sap/m/Text"
+
+], function (Text) {
+	"use strict";
+
+	new Text({
+		text: "Hello World Test Oliver"
+	}).placeAt("content");
+
+});
