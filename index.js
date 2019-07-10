790f69a303bd66c95b4fb74f133acddff1edaaed
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-10 12:09:06 +0200

    Test

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ sap.ui.define([
     "sap/m/Text",
     "sap/m/Link"
 
-], function (Text) {
-	"use strict";
-
-	new Text({
+], 
+function (Text,Link){
+    "use strict";
+    
+    new Text({
 		text: "Hello World"
 	}).placeAt("content_1");
-
-},
-function (Link){
-	"use strict";
-
 	new Link({
         text: "Testlink",
         href: "www.google.com"
