ab6fc15c4dea3175d68da47dd09c4880b7ff288c
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-12 08:21:27 +0200

    Test

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,4 +1,4 @@
-sap.ui.define([
+/*sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox"
  ], function (Controller, MessageBox) {
@@ -15,4 +15,14 @@ sap.ui.define([
 		);
        }
     });
- });
+ });*/
+ sap.ui.define(["sap/m/MessageBox"], function(MessageBox) {
+    MessageBox.show(
+        "This message should appear in the message box.", {
+            icon: MessageBox.Icon.INFORMATION,
+            title: "My message box title",
+            actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+            onClose: function(oAction) { / * do something * / }
+        }
+    );
+});
