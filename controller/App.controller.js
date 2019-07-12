823975962aa96bb97c8d1de10e51c01e5c806a69
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-12 08:58:56 +0200

    JSON

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,9 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
  ], function (Controller, MessageBox) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
+        onInit : function () {
+            // set data model on view
+            var oData = {
+               recipient : {
+                  name : "World"
+               }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+         },
        onShowHello : function () {
           MessageBox.show(
 			"This message should appear in the message box.", {
