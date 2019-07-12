c525340079e5c1bff4e14a8599d6a5e44c5843b0
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-12 09:50:10 +0200

    Translate

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
  ], function (Controller, MessageBox, JSONModel) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
@@ -14,8 +15,18 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+            });
+             this.getView().setModel(i18nModel, "i18n");
          },
        onShowHello : function () {
+        // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
           MessageBox.show(
 			"This message should appear in the message box.", {
 				icon: MessageBox.Icon.INFORMATION,
