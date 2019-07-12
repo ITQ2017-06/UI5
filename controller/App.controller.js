af4978d2c4b2412c6d73a24be86bd936b7d33341
ITQ2017-06 <ITQ2017-06@cloud.bs-hassfurt.de>
2019-07-12 08:15:35 +0200

    MassageBox

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,11 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
  ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
        onShowHello : function () {
           MessageToast.show("Hello World");
+          MessageBox.show(
+			"This message should appear in the message box.", {
+				icon: MessageBox.Icon.INFORMATION,
+				title: "My message box title",
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				onClose: function(oAction) { / * do something * / }
+			}
+		);
        }
     });
  });
